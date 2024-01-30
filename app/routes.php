caa295f30b8e06c202695ce0d2e2fd41a440e7a3
shorouqsalahat <shorouqsalahat@hotmail.com>
2024-01-30 11:58:46 +0200

    sh

--- app/routes.php
+++ app/routes.php
@@ -2,34 +2,35 @@
 
 declare(strict_types=1);
 use Slim\App;
-use App\Application\Actions\Majd\Majd;
+use Slim\Psr7\Request;
 
+use App\Application\Actions\Majd\Majd;
 use App\Application\Actions\Majd\test;
 use App\Domain\BicBucStriim\AppConstants;
 use App\Application\Actions\amal\AddAction;
-use App\Application\Actions\Amir\AmirAction;
 //use App\Application\Actions\AsemYamak\addAction;
 
+use App\Application\Actions\Amir\AmirAction;
 use App\Application\Actions\AsemYamak\HalaTaher;
 use App\Application\Actions\AsemYamak\TestAction;
 use App\Application\Actions\HalaTaher\HalaAction;
-use App\Application\Actions\InamSataria\InamAction;
 
-use App\Application\Actions\Shorouq\SaveBookAction;
+use App\Application\Actions\InamSataria\InamAction;
 //use App\Application\Actions\AsemYamak\AddBookAction;
 
-use App\Application\Actions\Shorouq\TestActionShoq;
+use App\Application\Actions\Shorouq\SaveBookAction;
 
+use App\Application\Actions\Shorouq\TestActionShoq;
 use Psr\Http\Message\ResponseInterface as Response;
+
 use App\Application\Actions\AsemYamak\AddBookAction;
 
+
+
+
+
+
 use App\Application\Actions\LameesAbuZahid\TestActionL;
-
-
-
-
-
-
 use App\Application\Actions\LameesAbuZahid\AddBookActionL;
 use Slim\Interfaces\RouteCollectorProxyInterface as Group;
 use App\Application\Actions\LameesAbuZahid\SaveBookActionL;
