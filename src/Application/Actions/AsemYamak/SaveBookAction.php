106887fca9745092159e289940986b66471777e8
Asem Yamak <asem_yamak@hotmail.com>
2024-01-30 11:53:06 +0200

    validation

--- src/Application/Actions/AsemYamak/SaveBookAction.php
+++ src/Application/Actions/AsemYamak/SaveBookAction.php
@@ -3,6 +3,7 @@ declare(strict_types=1);
 namespace App\Application\Actions\AsemYamak;
 
 
+use App\RequestValidators\AsemYamak\AddBookRequestValidator;
 use Slim\Psr7\Response;
 use App\Application\Actions\Action;
 use App\Models\AsemYamak\TestModel;
@@ -10,9 +11,12 @@ use App\Models\AsemYamak\TestModel;
 class SaveBookAction extends Action {
     protected function action(): Response
     {
+
         $data = $this->request->getParsedBody();
+        $data = $this->requestValidatorFactory->make(AddBookRequestValidator::class)->validate($data);
+       
         TestModel::save($data['name'], $data['description']);
-        //dd("insert");
+      
         return $this->view->render(
             $this->response,
             'asem/add_book.twig'
